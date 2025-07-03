d3e61f9cb7c5d73cd93a221e5429a13183e26041
jaskamal2724 <jaskamalsingh7872@gmail.com>
2025-07-03 22:55:03 +0530

    travel-result page.tsx updated search params

--- app/travel-result/page.tsx
+++ app/travel-result/page.tsx
@@ -1,24 +1,21 @@
-
 import { redirect } from "next/navigation";
 import { TravelResult } from "./TravelResult";
 
-interface PageProps {
-  searchParams: {
-    data?: string;
-  };
-}
-
-export default function TravelResultPage({ searchParams }: PageProps) {
-  if (!searchParams.data) {
+export default function TravelResultPage({
+  searchParams,
+}: {
+  searchParams?: { data?: string };
+}) {
+  if (!searchParams?.data) {
     redirect("/");
   }
 
   let parsedData;
   try {
     parsedData = JSON.parse(searchParams.data);
-  } catch (err) {
-    console.log(err)
-    redirect("/"); // Handle invalid JSON by redirecting
+  } catch(error){
+    console.log(error)
+    redirect("/");
   }
 
   return <TravelResult {...parsedData} />;
